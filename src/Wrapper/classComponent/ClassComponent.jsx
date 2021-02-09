19e53a675b014e9780be63c6b050e9c877ca6e82
Dima <dimcom1010@gmail.com>
2021-02-09 22:03:35 +0300

    калькулятор брал Zpt

--- src/Wrapper/classComponent/ClassComponent.jsx
+++ src/Wrapper/classComponent/ClassComponent.jsx
@@ -5,7 +5,6 @@ const Calculator = (props) => {
 
     let [number1, addnumber1] = useState("")
     let [number, addnumber] = useState("")
-    let [isUseZpt, addIsUseZpt] = useState(false)
     let [action, addAction] = useState("")
     let [actionUse, addActionUse] = useState(0)
     let [resultValue, addresultValue] = useState("")
@@ -13,40 +12,40 @@ const Calculator = (props) => {
 
 
     useEffect(() => {
-        debugger
-          if (!number1 && !number && !action && !resultValue && actionUse===0) {
+            debugger
+            if (!number1 && !number && !action && !resultValue && actionUse === 0) {
 
-              addDispley("He!")
-          }else if (number1 && !number && !action && !resultValue && actionUse===0)
-              { debugger
-                  addDispley(number1)
-          }else if ( action && !resultValue)
-          { debugger
-              addDispley(`${number} ${action} ${number1}`)
-          }else if (action && resultValue && actionUse !==2)
-          { debugger
-              addDispley(resultValue)
-              addnumber(resultValue)
-              addnumber1("")
-              addresultValue("")
-          }else if (!action && resultValue  && actionUse===0)
-          { debugger
-              addDispley(resultValue)
-              addActionUse(0)
-              addnumber1(resultValue)
-              addnumber("")
-              addresultValue("")
-          }
-        }, [ number1,action,number,resultValue, actionUse]
+                addDispley("He!")
+            } else if (number1 && !number && !action && !resultValue && actionUse === 0) {
+                debugger
+                addDispley(number1)
+            } else if (action && !resultValue) {
+                debugger
+                addDispley(`${number} ${action} ${number1}`)
+            } else if (action && resultValue && actionUse !== 2) {
+                debugger
+                addDispley(resultValue)
+                addnumber(resultValue)
+                addnumber1("")
+                addresultValue("")
+            } else if (!action && resultValue && actionUse === 0) {
+                debugger
+                addDispley(resultValue)
+                addActionUse(0)
+                addnumber1(resultValue)
+                addnumber("")
+                addresultValue("")
+            }
+        }, [number1, action, number, resultValue, actionUse]
     )
     console.log("number1", parseFloat(number1));
-    console.log("number",parseFloat(number));
-    console.log("isUseZpt",isUseZpt);
-    console.log("action",action);
-    console.log("actionUse",actionUse);
-    console.log("resultValue",resultValue);
-    console.log("displey",displey);
+    console.log("number", parseFloat(number));
+    console.log("action", action);
+    console.log("actionUse", actionUse);
+    console.log("resultValue", resultValue);
+    console.log("displey", displey);
     console.log("______________")
+
 
     const nubrebUse = (x) => {
         actionUse === 1
@@ -54,29 +53,26 @@ const Calculator = (props) => {
             : summ(x)
     }
     const secondNumber = (x) => {
-        addIsUseZpt(false)
         addActionUse(2)
         summ(x)
     }
 
     const summ = (x) => {
         if (x === ",")
-            {ifZpt(x)
-            }else {
+        {ifZpt(x)
+        }else {
             addnumber1(String(number1 += x))
         }
     }
 
     const ifZpt = (x) => {
-        if (x === "," && !isUseZpt) {
-            addIsUseZpt(true)
+        if (number1.includes('.')) {
+            addnumber1(number1 += String(""))
+        } else {
             addnumber1(String(
                 number1
-                    ?number1 += "."
-                    :number1 += "0."
-            ))
-        } else {
-            addnumber1(number1 += String(""))
+                    ? number1 += "."
+                    : number1 += "0."))
         }
     }
 
@@ -99,70 +95,65 @@ const Calculator = (props) => {
             addnumber1(number1 += String(""))
         }
     }
+
+
     const functions = {
 
         '+': (x, y) => parseFloat(x) + parseFloat(y),
         '-': (x, y) => parseFloat(x) - parseFloat(y),
         '*': (x, y) => parseFloat(x) * parseFloat(y),
         '/': (x, y) => parseFloat(y)
-                    ? parseFloat(x) / parseFloat(y)
-                    : "на ноль делить нельзя",
-        '%': (x, y) => parseFloat(x) /100 * parseFloat(y)
+            ? (parseFloat(x) / parseFloat(y)).toFixed(6)
+            : "на 0 делить нельзя",
+        '%': (x, y) => parseFloat(x) / 100 * parseFloat(y)
     };
 
     const result = (a, x, y, actionFrom) => {
         debugger
 
 
-        if(a && x && y && actionFrom==="equals") {
-            debugger
+        if (a && x && y && actionFrom === "equals") {
             addresultValue(String(functions[a](x, y)))
-            debugger
             addAction("")
-            debugger
             addActionUse(0)
 
-        } else if(a && x && y && actionFrom!=="equals"){
-            debugger
+        } else if (a && x && y && actionFrom !== "equals") {
             addresultValue(String(functions[a](x, y)))
-            debugger
             addAction(actionFrom)
-            debugger
             addActionUse(1)
 
         }
     }
-    const rec=()=>{
+    const rec = () => {
         addnumber1("")
         addnumber("")
-        addIsUseZpt(false)
         addAction(null)
         addActionUse(0)
         addDispley("")
         addresultValue("")
     }
 
-    const inversion =()=>{
-        addnumber1 ( String(parseFloat(number1)*(-1)) )
+    const inversion = () => {
+        addnumber1(String(parseFloat(number1) * (-1)))
     }
-    const backspace= ()=>{
-        let last =number1.slice(-1)
-        if (last===".") {
+    const backspace = () => {
+        let last = number1.slice(-1)
+        if (last === ".") {
             addnumber1(number1.slice(0, -1))
-            addIsUseZpt(false)
         } else {
             addnumber1(number1.slice(0, -1))
         }
     }
 
- /*   let display = React.createRef()*/
-  /*  let textDisplay=display.current.value*/
+    /*   let display = React.createRef()*/
+    /*  let textDisplay=display.current.value*/
 
     return (
         <div className={style.calculator_wrapper}>
             <div className={style.calculator_grid_body}>
 
-                <input className={style.grid_item + " " + style.dipley_input} type="text" onChange="alert(this.value)" value={displey}/>
+                <input className={style.grid_item + " " + style.dipley_input} type="text" onChange="alert(this.value)"
+                       value={displey}/>
 
                 <span className={style.grid_item + " " + style.item_span}>Super power 2000</span>
 
@@ -202,11 +193,9 @@ const Calculator = (props) => {
 
                 <button className={style.grid_item} onClick={() => nubrebUse(",")}>,</button>
 
-                <button className={style.grid_item} onClick={() => result(action, number, number1,"equals")}>=</button>
+                <button className={style.grid_item} onClick={() => result(action, number, number1, "equals")}>=</button>
 
                 <button className={style.grid_item} onClick={() => onAction("+")}>+</button>
-
-
 
 
             </div>
